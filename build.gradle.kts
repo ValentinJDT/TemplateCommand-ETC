33c7b759e00a19c6b510f1b72aaf7249d1feea10
VJT <vjeandot@yahoo.com>
2023-05-05 11:24:48 +0200

    remove main file and change version of kotlin jvm plugin and jdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,18 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
-    application
+    kotlin("jvm") version "1.8.20"
 }
 
 group = "fr.valentin.plugin.command"
-version = "1.5"
+version = "1.6"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation(files("dependencies/ValLib-0.1.0.jar"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
-    jvmToolchain(8)
-}
-
-application {
-    mainClass.set("MainKt")
+    jvmToolchain(17)
 }
