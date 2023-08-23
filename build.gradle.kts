a41b64c27a8225f7ba41f63a22272a91cd2c64a3
Valentin JEANDOT <valentin.jeandot@cirly.com>
2023-08-23 10:40:05 +0200

    remove jar dependencies and use jitpack instead

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.20"
+    application
 }
 
 group = "fr.valentin.plugin.command"
@@ -7,12 +8,43 @@ version = "1.6.2"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
 }
 
 dependencies {
-    implementation(files("dependencies/ValLib-0.1.2.jar"))
+    implementation("com.github.ValentinJDT:ValLib:v0.1.3")
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+val globalMainClass = (group as String) + ".MainKt"
+
+application {
+    mainClass.set(globalMainClass)
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = globalMainClass
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+
+    val list = mutableListOf("kotlin-stdlib-1.8.20.jar", "ValLib-v0.1.3.jar")
+
+    from({
+        configurations.runtimeClasspath.get().filter { jar ->
+
+            println(jar.path)
+
+            return@filter list.contains(jar.name)
+        }.map { zipTree(it) }
+    })
 }
